207d724c3d1927db41be16c6626d515b06a1cf14
Takunda Kangai <kangaitakunda@gmail.com>
2024-03-15 19:25:08 +0200

    Edit script.js to include a function that plays a round of Rock-Paper-Scissors.

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 const options = ["Rock", "Paper", "Scissors"];
 /*begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. */
 function getComputerChoice() {
-const choice = options[Math.floor(Math.random()*options.length)];
-return choice;
+const compChoice = options[Math.floor(Math.random()*options.length)];
+return compChoice;
+}
+/*Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
+Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).*/
+const playerSelection = "rOcK";
+const computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+const modPlayerSelection = playerSelection.toLowerCase();
+const modComputerSelection = computerSelection.toLowerCase();
+let msg ="";
+
+if (modPlayerSelection === "rock" && modComputerSelection === "rock"){
+    msg = "Stalemate! We have a draw.";
+    } else if (modPlayerSelection === "rock" && modComputerSelection === "paper") {
+    msg = "You Lose! Paper beats Rock."
+    } else if (modPlayerSelection === "rock" && modComputerSelection === "scissors") {
+    msg = "You Win! Rock beats Scissors."
+    } else if (modPlayerSelection === "paper" && modComputerSelection === "paper") {
+    msg = "Stalemate! We have a draw.";
+    } else if (modPlayerSelection === "paper" && modComputerSelection === "scissors") {
+    msg = "You Lose! Scissors beats Paper.";
+    } else if (modPlayerSelection === "paper" && modComputerSelection === "rock") {
+    msg = "You Win! Paper beats Rock.";
+    } else if (modPlayerSelection === "scissors" && modComputerSelection === "scissors") {
+    msg = "Stalemate! We have a draw.";
+    } else if (modPlayerSelection === "scissors" && modComputerSelection === "rock") {
+    msg = "You Lose! Rock beats Scissors.";
+    } else if (modPlayerSelection === "scissors" && modComputerSelection === "paper") {
+    msg = "You Win! Scissors beats Paper.";
+    } else if (modPlayerSelection === "") {
+    msg = "Enter something.";
+    } else {
+    msg = "";    
+    }
+
+return msg;
 }
 
-console.log(getComputerChoice());
+console.log(playerSelection.toLowerCase() + " : " + computerSelection.toLowerCase());
+console.log(playRound(playerSelection, computerSelection));
