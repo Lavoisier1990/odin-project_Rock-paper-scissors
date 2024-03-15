e5874d5815ae4f07d37a06c27fb5f089c665b0c4
Takunda Kangai <kangaitakunda@gmail.com>
2024-03-15 20:05:02 +0200

    Edit script.js to include the prompt for user input.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ return compChoice;
 }
 /*Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
 Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).*/
-const playerSelection = "rOcK";
+const playerSelection = prompt("Choose your weapon");
 const computerSelection = getComputerChoice();
 
 
@@ -44,3 +44,9 @@ return msg;
 
 console.log(playerSelection.toLowerCase() + " : " + computerSelection.toLowerCase());
 console.log(playRound(playerSelection, computerSelection));
+
+/*Write a NEW function called playGame(). Use the previous function inside of this one to play a five round game that keeps score and reports a winner or loser at the end. Use prompt() to get input from the user.*/
+
+function playGame(){
+
+}
