c27705d1e36815624e70c11d3745013707195aa4
Takunda Kangai <kangaitakunda@gmail.com>
2024-03-16 01:58:57 +0200

    Add a button to the html markup to initialise the game. Edit script.js to accommodate the addition of the button.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ return compChoice;
 }
 /*Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
 Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).*/
+
+function myFunction(string){
+    string = "";
+
 const playerSelection = prompt("Choose your weapon");
 const computerSelection = getComputerChoice();
 
@@ -36,7 +40,7 @@ if (modPlayerSelection === "rock" && modComputerSelection === "rock"){
     } else if (modPlayerSelection === "") {
     msg = "Enter something.";
     } else {
-    msg = "";    
+    msg = "Choose among 'rock', 'paper' or 'scissors'.";    
     }
 
 return msg;
@@ -44,8 +48,13 @@ return msg;
 
 console.log(playerSelection.toLowerCase() + " : " + computerSelection.toLowerCase());
 console.log(playRound(playerSelection, computerSelection));
-
+}
 /*Write a NEW function called playGame(). Use the previous function inside of this one to play a five round game that keeps score and reports a winner or loser at the end. Use prompt() to get input from the user.*/
+
+let playerScore = 0;
+let compScore = 0;
+let gameOver = false;
+let numberOfGames = 5;
 
 function playGame(){
 
