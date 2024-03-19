ed96f3803b4b375bbde50bb1f4b55254164c3aad
Takunda Kangai <kangaitakunda@gmail.com>
2024-03-19 03:30:33 +0200

    Edit script.js to add the playGame function. Edit Rock-paper-scissors.html to change the button onclick attribute to accomodate this change.

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ return compChoice;
 /*Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
 Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).*/
 
-function myFunction(string){
-    string = "";
+function playGame(){
+    myFunction();
+    myFunction();
+    myFunction();
+    myFunction();
+    myFunction();
+}
 
+function myFunction(){
 const playerSelection = prompt("Choose your weapon");
 const computerSelection = getComputerChoice();
 
@@ -28,51 +34,50 @@ if (modPlayerSelection === "rock" && modComputerSelection === "rock"){
     } else if (modPlayerSelection === "rock" && modComputerSelection === "paper") {
         playerScore; 
         compScore += 3; 
-    msg = `You Lose! Paper beats Rock.\n ${playerScore} : ${compScore}`;
+    msg = `${modPlayerSelection} : ${modComputerSelection}\n You Lose! Paper beats Rock.\n ${playerScore} : ${compScore}`;
     } else if (modPlayerSelection === "rock" && modComputerSelection === "scissors") {
         playerScore += 3; 
         compScore; 
-    msg = `You Win! Rock beats Scissors.\n ${playerScore} : ${compScore}`;
+    msg = `${modPlayerSelection} : ${modComputerSelection}\n You Win! Rock beats Scissors.\n ${playerScore} : ${compScore}`;
     } else if (modPlayerSelection === "paper" && modComputerSelection === "paper") {
         playerScore += 1; 
         compScore += 1; 
-    msg = `Stalemate! We have a draw.\n ${playerScore} : ${compScore}`;
+    msg = `${modPlayerSelection} : ${modComputerSelection}\n Stalemate! We have a draw.\n ${playerScore} : ${compScore}`;
     } else if (modPlayerSelection === "paper" && modComputerSelection === "scissors") {
         playerScore; 
         compScore += 3;
-    msg = `You Lose! Scissors beats Paper.\n ${playerScore} : ${compScore}`;
+    msg = `${modPlayerSelection} : ${modComputerSelection}\n You Lose! Scissors beats Paper.\n ${playerScore} : ${compScore}`;
     } else if (modPlayerSelection === "paper" && modComputerSelection === "rock") {
         playerScore += 3; 
         compScore; 
-    msg = `You Win! Paper beats Rock.\n ${playerScore} : ${compScore}`;
+    msg = `${modPlayerSelection} : ${modComputerSelection}\n You Win! Paper beats Rock.\n ${playerScore} : ${compScore}`;
     } else if (modPlayerSelection === "scissors" && modComputerSelection === "scissors") {
         playerScore += 1; 
         compScore += 1; 
-    msg = `Stalemate! We have a draw.\n ${playerScore} : ${compScore}`;
+    msg = `${modPlayerSelection} : ${modComputerSelection}\n Stalemate! We have a draw.\n ${playerScore} : ${compScore}`;
     } else if (modPlayerSelection === "scissors" && modComputerSelection === "rock") {
         playerScore; 
         compScore += 3;
-    msg = `You Lose! Rock beats Scissors.\n ${playerScore} : ${compScore}`;
+    msg = `${modPlayerSelection} : ${modComputerSelection}\n You Lose! Rock beats Scissors.\n ${playerScore} : ${compScore}`;
     } else if (modPlayerSelection === "scissors" && modComputerSelection === "paper") {
         playerScore += 3; 
         compScore; 
-    msg = `You Win! Scissors beats Paper.\n ${playerScore} : ${compScore}`;
+    msg = `${modPlayerSelection} : ${modComputerSelection}\n You Win! Scissors beats Paper.\n ${playerScore} : ${compScore}`;
     } else if (modPlayerSelection === "") {
         playerScore;
         compScore; 
-    msg = `Enter something.\n ${playerScore} : ${compScore}`;
+    msg = `${modPlayerSelection} : ${modComputerSelection}\n Enter something.\n ${playerScore} : ${compScore}`;
     } else {
         playerScore;
         compScore; 
-    msg = `Choose among 'rock', 'paper' or 'scissors'.\n ${playerScore} : ${compScore}`;    
+    msg = `${modPlayerSelection} : ${modComputerSelection}\n Choose among 'rock', 'paper' or 'scissors'.\n ${playerScore} : ${compScore}`;    
     }
-
 return msg;
 }
-
-console.log(playerSelection.toLowerCase() + " : " + computerSelection.toLowerCase());
 console.log(playRound(playerSelection, computerSelection));
 }
+
+
 /*Write a NEW function called playGame(). Use the previous function inside of this one to play a five round game that keeps score and reports a winner or loser at the end. Use prompt() to get input from the user.*/
 
 /*let playerScore = 0;
